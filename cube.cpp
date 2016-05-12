6deacc14820f8becfae945d57374ea9b82d87afb
gha-gozu <ghazwan.sihamudin@gmail.com>
2016-05-12 10:06:14 +0700

    added directional and spotlight lighting with ambient, diffuse, and specular light

--- cube.cpp
+++ cube.cpp
@@ -531,6 +531,26 @@ void specialKeys( int key, int a, int b ) {
   glutPostRedisplay();
  
 }
+
+// ----------------------------------------------------------
+// light properties
+// ----------------------------------------------------------
+const GLfloat light_ambient[]  = { 0.0f, 0.2f, 1.0f, 1.0f };
+const GLfloat light1_ambient[]  = { 0.3f, 0.2f, 0.0f, 1.0f };
+const GLfloat light_diffuse[]  = { 1.0f, 1.0f, 0.0f, 1.0f };
+const GLfloat light1_diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat light1_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat light_position[] = { -5.0f, 10.0f, 10.0f, 0.0f };
+const GLfloat light1_position[] = { 5.0f, 10.0f, 5.0f, 0.0f };
+const GLfloat spot_direction[] = { -10.0f, 10.0f, 0.0};
+
+const GLfloat mat_ambient[]    = { 0.7f, 0.7f, 0.7f, 1.0f };
+const GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 0.0f };
+const GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat high_shininess[] = { 100.0f };
+
+
  
 // ----------------------------------------------------------
 // main() function
@@ -548,6 +568,27 @@ int main(int argc, char* argv[]){
  
   //  Enable Z-buffer depth test
   glEnable(GL_DEPTH_TEST);
+  glDepthFunc(GL_LESS);
+
+  glEnable(GL_LIGHT0);
+  glEnable(GL_LIGHT1);
+  glEnable(GL_NORMALIZE);
+  glEnable(GL_COLOR_MATERIAL);
+  glEnable(GL_LIGHTING);
+
+
+  glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+  glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient);
+  glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_diffuse);
+  glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+
+  glLightfv(GL_LIGHT1, GL_POSITION, light1_position);
+  glLightfv(GL_LIGHT1, GL_AMBIENT,  light1_ambient);
+  glLightfv(GL_LIGHT1, GL_DIFFUSE,  light1_diffuse);
+  glLightfv(GL_LIGHT1, GL_SPECULAR, light_specular);
+  glLightf(GL_LIGHT1, GL_SPOT_CUTOFF, 45.0);
+  glLightfv(GL_LIGHT1, GL_SPOT_DIRECTION, spot_direction);
+
  
   // Callback functions
   glutDisplayFunc(display);
